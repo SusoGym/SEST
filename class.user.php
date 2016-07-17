4bb4422f55d7db98812177181bd05817d96ce07b
Parranoh <minerva.schlau@hotmail.com>
2016-07-17 15:56:40 +0200

    Added class.user.php.
    Contains a rough User class as well as a Parent class and a Teacher
    class. Not sure if it works the way I intended.

---
+++ class.user.php
@@ -0,0 +1,88 @@
+<?php
+
+require("class.model.php");
+
+/**
+ *
+ */
+class User
+{
+
+  function __construct($id, $set)
+  {
+    $model = new Model();
+    if !(isset($set)) {
+      $this->type = $model->user_get_type($id);
+      if ($this->type == 1) {
+        return new Parent($id, 0);
+      } elseif ($this->type == 2) {
+        return new Teacher($id, 0);
+      }
+    } else {
+      $this->id = $id;
+      $this->name = $model->user_get_name($id);
+    }
+  }
+
+  function get_id()
+  {
+    return $this->id;
+  }
+
+  function get_name()
+  {
+    return $this->name;
+  }
+
+  function get_type()
+  {
+    return $this->type;
+  }
+}
+
+
+/**
+ *
+ */
+class Parent extends User
+{
+
+  function __construct()
+  {
+    $model = new Model();
+    $this->children = $model->parent_get_children($this->id);
+  }
+
+  function get_children()
+  {
+    return $this->children;
+  }
+
+  function get_teachers()
+  {
+    $model = new Model();
+    foreach ($this->children as $key => $value) {
+      $classes[] = $model->student_get_class($value);
+    }
+    $teachers = array();
+    foreach ($classes as $key => $value) {
+      $teachers = array_merge($teachers, $model->class_get_teachers($value));
+    }
+    return $teachers;
+  }
+}
+
+
+/**
+ *
+ */
+class Teacher extends User
+{
+
+  function __construct()
+  {
+    # code...
+  }
+}
+
+?>
