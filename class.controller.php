e9b105422f298ea3f022bf70a9bbbda9767f26cf
Parranoh <minerva.schlau@hotmail.com>
2016-07-17 16:33:57 +0200

    More work done.
    Basis for booking system laid out.

--- class.controller.php
+++ class.controller.php
@@ -8,30 +8,34 @@ class Controller
 
   function __construct($input)
   {
-	$model = new Model();
+	  $model = new Model();
+    if (isset($_SESSION['user'])) {
+      $this->user = new User($_SESSION['user']);
+    }
     if (isset($input['type'])) {
-	  if ($input['type'] == "login") {
-	    # validate, then set session var
-	  } elseif ($input['type'] == "booking") {
-        if ($input['booking_action'] == "add") {
-          # create new Booking, send to Model
-        } elseif ($input['booking_action'] == "delete") {
-          # exec
-        }
-      } elseif ($input['type'] == "register") {
-		# check, then write into database, then login (session var...)
-	  }
+  	  if ($input['type'] == "login") {
+  	    # validate, then set session var
+  	  } elseif ($input['type'] == "booking") {
+          if ($input['booking']['action'] == "add") {
+            $model->booking_add($input['booking']['slot'], $this->user->get_id(), $input['booking']['teacher']);
+          } elseif ($input['booking']['action'] == "delete") {
+            $model->booking_delete($input['booking']['slot'], $this->user->get_id());
+          }
+        } elseif ($input['type'] == "register") {
+  		# check, then write into database, then login (session var...)
+  	  }
 
     } else {
       $this->tpl = "login";
-	  $this->display();
+	    $this->display();
     }
   }
-  
+
   function display()
   {
-	$view = new View($this->tpl);
+	  $view = new View($this->tpl);
   }
 
 }
+
 ?>
