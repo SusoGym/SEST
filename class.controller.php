7b1fde53fc9f96c68e169a5c25c2daa525f19b80
Parranoh <minerva.schlau@hotmail.com>
2016-07-18 16:44:42 +0200

    switch case instead of if elseif elseif elseif else

--- class.controller.php
+++ class.controller.php
@@ -10,19 +10,33 @@ class Controller
   {
     $model = new Model();
     if (isset($_SESSION['user'])) {
-      $this->user = new User($_SESSION['user']);
+      if ($_SESSION['user']['type'] == 1) {
+        $this->user = new Parent($_SESSION['user']);
+      } elseif ($_SESSION['user']['type']) {
+        $this->user = new Teacher($_SESSION['user']);
+      }
     }
     if (isset($input['type'])) {
-      if ($input['type'] == "login") {
-  	# validate, then set session var
-      } elseif ($input['type'] == "booking") {
-        if ($input['booking']['action'] == "add") {
-          $model->booking_add($input['booking']['slot'], $this->user->get_id(), $input['booking']['teacher']);
-        } elseif ($input['booking']['action'] == "delete") {
-          $model->booking_delete($input['booking']['slot'], $this->user->get_id());
-        }
-      } elseif ($input['type'] == "register") {
-  	# check, then write into database, then login (session var...)
+      switch ($input['type']) {
+        case "login":
+  	      # validate, then set session var
+  	      break;
+  	      
+  	    case "booking":
+  	      if ($input['booking']['action'] == "add") {
+            $model->booking_add($input['booking']['slot'], $this->user->get_id(), $input['booking']['teacher']);
+          } elseif ($input['booking']['action'] == "delete") {
+            $model->booking_delete($input['booking']['slot'], $this->user->get_id());
+          }
+          break;
+          
+        case "register":
+  	      # check, then write into database, then login (session var...)
+  	      break;
+  	      
+  	    default:
+  	      $this->tpl = "login";
+          $this->display();
       }
 
     } else {
