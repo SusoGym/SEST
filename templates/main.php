9e8963db06a57df0d537c04691fa2a78d9342b95
DrKongoIV <mail@jasperkrauter.de>
2016-07-18 16:03:40 +0200

    added template "main"

---
+++ templates/main.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>SUSO ESP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="http://materializecss.com/bin/materialize.css"  media="screen,projection"/>
+  </head>
+  <body class="grey lighten-2">
+
+    <nav>
+      <div class="nav-wrapper teal">
+        <a href="#" style="margin-left: 20px" class="brand-logo">Elternsprechtag</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="">Eintragen</a></li>
+          <li><a href="">Termine anzeigen</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      <div class="card ">
+        <div class="card-content">
+          <span class="card-title">Card Title</span>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div class="card-action">
+          <a href="#">This is a link</a>
+          <a href="#">This is a link</a>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="http://materializecss.com/bin/materialize.js"></script>
+  </body>
+</html>
