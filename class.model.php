aa2cba9054a4da229048c0cab84b6aa3fee25748
phartleitner <hartleitner@suso.schulen.konstanz.de>
2016-07-18 17:08:58 +0200

    Update class.model.php

--- class.model.php
+++ class.model.php
@@ -5,11 +5,31 @@
  */
 class Model
 {
+  private $connection;//Connection Object
 
+  /**
+  *Konstruktor
+  */
   function __construct()
   {
-    # code...
+    $this->connection=new Connect();
   }
+  
+  
+  /**
+   * Schülerexistenz prüfen
+   * 
+   * /
+   public function checkPupilExist($name,$bday){
+   $id=null;
+   $name=$this->connection->mysqli_real_escape_string($name);
+   $bday=$this->connection->mysqli_real_escape_string($bday);
+   $data=$this->connection->selectValues('SELECT id FROM schueler WHERE Name="$name" and bday="$bday" AND EId=null');
+   if(isset($datat[0][0])) $id=$data[0][0];
+   return $id;
+   }
+  
+  
 }
 
 
