3f55f3869e94131afd48cbe1b297682e84127838
phartleitner <hartleitner@suso.schulen.konstanz.de>
2016-10-31 10:31:37 +0100

    admin login detection & minor changes
    login logic checks if logged in username is of type 0, i.e. admin

--- administrator/templates/update2.php
+++ administrator/templates/update2.php
@@ -28,7 +28,7 @@
     
 
    
-	  
+	 <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>  
 	<script type="text/javascript" src="http://materializecss.com/bin/materialize.js"></script>
     <script>
         $(document).ready(function(){
