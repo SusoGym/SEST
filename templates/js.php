de77c832779ada5be63c4e1ef8cbea2c72b35c91
DrKongoIV <mail@jasperkrauter.de>
2016-12-13 23:24:51 +0100

    light changes in templates "parent_child_select" and "parent_dashboard"
    also made modals dismissible, didn't find where this schould be not
    given but while adding a student this is very annoying...

--- templates/js.php
+++ templates/js.php
@@ -84,7 +84,7 @@
 
     function initModal() {
         $('.modal').modal({
-                dismissible: false, // Modal can be dismissed by clicking outside of the modal
+                dismissible: true, // Modal can be dismissed by clicking outside of the modal
                 opacity: .5, // Opacity of modal background
                 in_duration: 300, // Transition in duration
                 out_duration: 200, // Transition out duration
