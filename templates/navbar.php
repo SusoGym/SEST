ab404720b94eae1015a1f6b79e2723ede45ed67e
smephite <smephite@yahoo.com>
2016-12-16 19:19:29 +0100

    Redone navbar in a more modular way [*] Now uses foreach to create <li> in navbar with information [*] Fixes error when logged in as teacher as $data['children'] is non existent [*] Now displays correct links to parent / teacher specific locations

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,22 +1,27 @@
 <?php
     $data = $this->getDataForView();
-    $children = $data['children'];
+
     $selectionActive = $est = $vplan = $events = $news = true;
     $today = date("Ymd");
-    if (isset($_SESSION['user']['id']))
+
+    $userObj = Controller::getUser();
+
+    if ($userObj instanceof Guardian)
     {
-        $userObj = Controller::getUser();
+        $children = $data['children'];
+        if (count($children) == 0)
+        {
+            $est = false;
+            $selectionActive = false;
+        }
     }
-    if ((isset($data['book_end']) && $data['book_end'] < $today) || (isset($data['book_start']) && $data['book_start'] > $today) || count($children) == 0)
+
+    if ($est && (isset($data['book_end']) && $data['book_end'] < $today) || (isset($data['book_start']) && $data['book_start'] > $today))
     {
         ChromePhp::info("No children selected by guardian or booking time expired");
         $est = false;
     }
 
-    if (count($children) == 0)
-    {
-        $selectionActive = false;
-    }
 
     $color = array(true => 'white-text', false => 'teal-text text-lighten-3');
 
@@ -36,28 +41,42 @@
             $news = false;
         }
     }
+
+
+    $modules = array();
+
+    array_push($modules, array("id" => "home", "href" => ".", "title" => "Home", "icon" => "home", "inner" => "<font style='font-size: 24px;'>Suso-Intern</font>"));
+
+    if ($userObj instanceof Guardian)
+    {
+
+        array_push($modules, array("id" => "est", "href" => $est ? "?type=eest" : "", "title" => "Elternsprechtag", "icon" => "supervisor_account", "classes" => $color[$est]));
+
+    } else
+    {
+
+        array_push($modules, array("id" => "est", "href" => $est ? "?type=lest" : "", "title" => "Elternsprechtag", "icon" => "supervisor_account", "classes" => $color[$est]));
+
+    }
+
+    array_push($modules, array("id" => "vplan", "href" => $vplan ? "?type=vplan" : "", "title" => "Vertretungsplan", "icon" => "dashboard", "classes" => $color[$vplan]));
+    array_push($modules, array("id" => "events", "href" => $events ? "?type=events" : "", "title" => "Termine", "icon" => "today", "classes" => $color[$events]));
+    array_push($modules, array("id" => "news", "href" => $news ? "?type=news" : "", "title" => "Newsletter", "icon" => "library_books", "classes" => $color[$news]));
+
+    foreach ($modules as $module)
+    {
+        $id = $module['id'];
+        $link = $module['href'];
+        $title = $module['title'];
+        $icon = $module['icon'];
+        $inner = (isset($module['inner'])) ? $module['inner'] : $title;
+        $classes = (isset($module['classes'])) ? $module['classes'] : "";
+
+        ?>
+        <li><a id="<?php echo $id ?>" href="<?php echo $link ?>" title="<?php echo $title ?>"
+               class="<?php echo $classes ?>"><i
+                        class="material-icons left"><?php echo $icon ?></i><?php echo $inner ?></a></li>
+        <?php
+    }
+
 ?>
-
-
-<li><a id="home" href="." title="Home"><i class="material-icons left">home</i><font
-                style="font-size: 24px;">Suso-Intern</font></a></li>
-<li><a id="childsel" href="?type=childsel" title="Home"><i class="material-icons left">face</i>Kinder verwalten</a></li>
-<li><a id="est" <?php if ($est)
-    {
-        echo 'href="?type=eest"';
-    } ?> title="Home" class="<?php echo $color[$est]; ?>"><i class="material-icons left">supervisor_account</i>Elternsprechtag</a>
-</li>
-<li><a id="vplan" <?php if ($vplan)
-    {
-        echo 'href="?type=vplan"';
-    } ?> title="Home" class="<?php echo $color[$vplan]; ?>"><i class="material-icons left">dashboard</i>Vertretungsplan</a>
-</li>
-<li><a id="events" <?php if ($events)
-    {
-        echo 'href="?type=events"';
-    } ?> title="Home" class="<?php echo $color[$events]; ?>"><i class="material-icons left">today</i>Termine</a></li>
-<li><a id="news" <?php if ($news)
-    {
-        echo 'href="?type=news"';
-    } ?> title="Home" class="<?php echo $color[$news]; ?>"><i
-                class="material-icons left">library_books</i>Newsletter</a></li>
