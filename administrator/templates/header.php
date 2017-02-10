82cdc1612a54739f612a5ff520b2828633d55822
smephite <smephite@yahoo.com>
2017-02-10 16:24:13 +0100

    Updated external links in /administrator to use https over http

--- administrator/templates/header.php
+++ administrator/templates/header.php
@@ -7,9 +7,8 @@
   <title>Adminpanel</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="icon" type="image/ico" href="../favicon.ico">
-  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link type="text/css" rel="stylesheet" href="http://materializecss.com/bin/materialize.css"
-  media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/css/materialize.min.css">
 
   <script type="application/javascript"> // not that nice here but where else?
 
