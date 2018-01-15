9beeb6c03b5eba61e1c9936fa28943756a941861
phartleitner <hartleitner@suso.schulen.konstanz.de>
2018-01-15 19:38:39 +0100

    adding url to link
    reaction to user reports that password reset links do not work

--- templates/resetmail.php
+++ templates/resetmail.php
@@ -1,7 +1,19 @@
-<?php if (!isset($url)) $url = "#"; ?>
+<?php 
+$url = (!isset($url) ) ?  "#" : "https://www.".$url; 
+
+
+?>
+<html>
+<head>
+</head>
+<body>
 <div style="font-family: sans-serif; text-align: left;">
-    <p>Bitte folgen Sie <a href="<?php echo $url; ?>">diesem Link</a>, um Ihr Passwort zurückzusetzen. Er verliert seine
+    <p>Bitte folgen Sie dem Link <a href="<?php echo $url; ?>"><?php echo $url; ?></a>, um Ihr Passwort zurückzusetzen. Er verliert seine
        Gültigkeit in 24 Stunden, sollten Sie bis dahin Ihr Passwort nicht zurückgesetzt haben, müssen Sie einen neuen
        Link anfordern.</p>
+    <p><br/>Sollte obiger Link nicht funktionieren, kopieren Sie bitte folgende Zeile in die Adresszeile Ihres Browsers, um das Passwort zurückzusetzen:<br/><br/>
+  	<?php echo $url; ?><br/></p>
     <p>Mit freundlichen Grüßen,<br>Ihr Suso-Intern-Team</p>
 </div>
+</body>
+</html>
