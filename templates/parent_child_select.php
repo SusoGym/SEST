98f7036e2ddd80223255abb8083bc977d853b679
phartleitner <hartleitner@suso.schulen.konstanz.de>
2018-03-30 23:38:22 +0200

    changed student registration for parents
    enhanced security - parents can only regitetr student via registration
    key (needs to be part of student data in database)

--- templates/parent_child_select.php
+++ templates/parent_child_select.php
@@ -3,6 +3,7 @@
 //$model = Model::getInstance();
 $data = $this->getDataForView();
 $students = $data['children'];
+$user = $data['user'];
 include("header.php");
 
 ?>
@@ -17,9 +18,9 @@ include("header.php");
 					<a id="backButton" class="mdl-navigation__link waves-effect waves-light teal-text" href=".">
 						 <i class="material-icons">chevron_left</i>
 					</a>
-					Bitte Kinder angeben:
+					Bitte registrieren Sie Ihr Kind:
 				</span>
-                <a style="position: absolute; bottom:20px; right:20px;" class="btn-floating btn-large teal"
+				<a style="position: absolute; bottom:20px; right:20px;" class="btn-floating btn-large teal"
                    href="#addstudent"><i class="material-icons">add</i></a>
             <?php }
             else
@@ -41,6 +42,7 @@ include("header.php");
                     <?php } ?>
                 </ul>
                 <?php } ?>
+            <span style="font-size=10px;">Sie benötigen zur Registrierung weiterer Kinder einen Registrierungsschlüssel! <br><a href="#requirekey" class="teal-text text-darken-2">Ich habe keinen Registrierungsschüssel oder kenne das nicht!</a></span>
             
             </div>
         </div>
@@ -65,16 +67,41 @@ include("header.php");
     </div>
 </div>
 
+<div id="requirekey" class="modal">
+    <div class="modal-content">
+        <h4>Registrierungsschlüssel anfordern</h4>
+        <div class="row">
+		Aus Sicherheitsgründen können Sie Ihre/e Kind/er nur mittels einesSchlüssels registrieren, 
+		welchen Sie mit der Anmeldebestätigung erhalten haben. Sie können diesen erneut anfordern.
+		Tragen Sie hierzu bitte die geforderten Daten ein.
+		Der Schlüssel wird Ihnen dann an die hinterlegte Emailadresse gesendet.
+		</div>
+		<div class="row">
+			<div class="input-field col l4 m4 s4">
+				<input id="name" name="name" type="text" class="validate">
+				<label for="name" class="truncate">Name des Kindes</label>
+			</div>
+			<div class="input-field col l4 m4 s4">
+				<input id="klasse" name="klasse" type="text" class="validate">
+				<label for="klasse" class="truncate">Klasse</label>
+			</div>
+			<div class="input-field col l4 m6 s4">
+				<input id="bday" type="date" name="bday" class="datepicker">
+				<label for="date" class="truncate">Geburtstag</label>
+			</div>   
+        </div>
+        <a onclick="submitKeyRequestForm();" class="modal-action waves-effect waves-green btn-flat right teal-text"
+           style="margin-bottom: 20px;"><i class="material-icons right">send</i>Jetzt anfordern</a>
+    </div>
+</div>
+
 
 <div id="student_blueprint" style="display:none;">
-    <div class="input-field col l6 m6 s6">
-        <input id="name" name="name" type="text" class="validate">
-        <label for="name" class="truncate">Name des Schülers</label>
+    <div class="input-field col l12 m12 s12">
+        <input id="id" name="id" type="text" class="validate">
+        <label for="name" class="truncate">Registrierungscode</label>
     </div>
-    <div class="input-field col l6 m6 s6">
-        <input type="date" name="bday" class="datepicker">
-        <label for="date" class="truncate">Geburtstag</label>
-    </div>
+    
 </div>
 
 <?php include("js.php"); ?>
@@ -82,21 +109,21 @@ include("header.php");
 <script type="application/javascript">
     function submitStudentForm() {
 		var url_param = "?console&type=addstudent";
-
         var studentNodes = document.getElementsByClassName("student_instance");
-
+		var students = [];
         var numValidStudents = 0;
         for (var i = 0; i < studentNodes.length; i++) {
             var student = studentNodes[i];
-            var name = student.childNodes[1].childNodes[1].value;
-            var bday = student.childNodes[3].childNodes[1].value;  // magic numbers op!
-            if (name == "" || bday == "")
+            var id = student.childNodes[1].childNodes[1].value;
+			//console.info(student.childNodes[1].childNodes[1].value);
+            if (id == "" )
                 continue;
-            name = name.replace(/\s/g, '');
+            //students.push(id);
             numValidStudents++;
-            url_param += "&students[]=" + name + ":" + bday;
+            url_param += "&students[]=" + id;
         }
-
+		//url_param += "&students[]=" + students;
+		console.info(url_param);
         if (numValidStudents == 0) {// No valid Students...
             Materialize.toast("Bitte geben sie mindestens einen Schüler an.",4000);
             return;
@@ -126,7 +153,48 @@ include("header.php");
     }
 
     var counter = 0;
-
+	function submitKeyRequestForm() {
+	var url_param = "?console&type=requestkey";	
+	name = $('#name').val();
+	klasse = $('#klasse').val(); 
+	bday = $('#bday').val();
+	userEmail = "<?php echo $user->getEmail(); ?>";
+	if (name == "" || klasse == "" || bday == "") {
+		Materialize.toast("Bitte geben sie die Daten vollständig ein.",4000);
+		} else {
+		$.post("index.php" + url_param, {email:userEmail,student:name,kl:klasse,dob:bday},function (data) {
+            try {
+                //var myData = JSON.parse(data);
+				//console.info(data);
+                if (data.success) {
+                    //location.reload();
+					Materialize.toast("Ihre Anfrage wird bearbeitet!", 4000);
+					//reset form
+					$('#name').val('');
+					$('#name').blur();
+					$('#klasse').val('');
+					$('#klasse').blur();
+					$('#bday').val('');
+					$('#bday').blur();
+					$('#requirekey').trigger('reset');
+					$('#requirekey').modal('close')
+                }
+                else { // oh no! ;-;
+					var notifications = data.notifications; //myData['notifications'];
+                    notifications.forEach(function (data) {
+                        Materialize.toast(data, 4000);
+                    });
+                }
+            } catch (e) {
+                Materialize.toast('Interner Server Fehler!',4000);
+                console.error(e);
+                console.info('Request: ' + url_param);
+                console.info('Response: ' + data);
+            }
+        });
+		}
+	
+	}
     function addStudent() {
         counter++;
         if (counter <= 100) {
